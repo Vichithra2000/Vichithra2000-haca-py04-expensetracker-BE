b5fc2b0146e32e88ca76768cce9eda88de0f9e25
Vichithra2000 <vichithra123@gmail.com>
2026-07-08 18:55:59 +0530

    allowed hosts updated

--- expense_tracker_backend/settings.py
+++ expense_tracker_backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-^p0t12lcdy6gri^5i^o783uy1o@iizd6)ob7qvvr5=4f))sfct
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["Vichithrasajeev.pythonanywhere.com"]
 
 
 # Application definition

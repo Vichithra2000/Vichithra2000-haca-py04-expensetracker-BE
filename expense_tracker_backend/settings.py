52a7b1ab712724b1daee0f3643fa35cd2654e07d
Vichithra2000 <vichithra123@gmail.com>
2026-08-15 15:51:18 +0530

    update settings

--- expense_tracker_backend/settings.py
+++ expense_tracker_backend/settings.py
@@ -130,7 +130,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(seconds=20),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
     "AUTH_HEADER_TYPES": ("Bearer",),
 }
 
